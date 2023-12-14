b40fca71689b309910536b4b5df7271e1898f020
Nurislam <znurislama56@gmail.com>
2023-12-14 16:07:05 +0300

    refactor properties, AppConfig, pom.xml upgrade minor version of dependencies,

--- src/main/java/ru/shop/backend/search/config/AppConfig.java
+++ src/main/java/ru/shop/backend/search/config/AppConfig.java
@@ -2,46 +2,20 @@ package ru.shop.backend.search.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.apache.http.Header;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.info.BuildProperties;
-import  org.springframework.http.HttpHeaders;
-import org.apache.http.message.BasicHeader;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableElasticsearchRepositories
 @EnableScheduling
-public class AppConfig implements WebMvcConfigurer {
-    private String elasticUrl = "127.0.0.1:9200";
-    @Bean
-    public ClientConfiguration clientConfiguration(){
-        return ClientConfiguration.builder().connectedTo(elasticUrl)
-                .build();
-    }
-    @Bean
-    @Autowired
-    public RestHighLevelClient restHighLevelClient(ClientConfiguration client){
-        return RestClients.create(client).rest();
-    }
-
+public class AppConfig {
     @Bean
     public OpenAPI openAPI(@Value("${springdoc.version}") String version) {
-        OpenAPI openAPI = new OpenAPI();
-        openAPI.info(new Info()
-                .title("Проект электронных очередей")
-                .version(version));
-        return openAPI;
+        return new OpenAPI()
+                .info(new Info()
+                        .version(version));
     }
-
-
 }
